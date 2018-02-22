bae5d124d79eceef3bb78d51baa89dc8baf658a7
Melanie <mlpatt23@gmail.com>
2018-02-22 16:06:27 -0700

    Fixed invalid header issue

--- src/simple-testimonials-by-factor1/inc/custom-functions.php
+++ src/simple-testimonials-by-factor1/inc/custom-functions.php
@@ -1,17 +1,8 @@
 <?php
 /*
-Plugin Name: Simple Testimonials by Factor1
-Plugin URI: https://github.com/factor1/simple-testimonials
-Description: A simple & straight forward WordPress plugin that allows you to create testimonials with citations and categories.
-Author: Eric Stout (Factor1)
-Version: 0.1.1
-Author URI: http://www.factor1studios.com
+* Custom functions 
+*
 */
-
-/**
- * @package Simple_Testimonials
- * @version 0.1.1
- */
 
 // Custom Excerpt function for Advanced Custom Fields (For Tesimonials Excerpts)
 function simple_testimonials_excerpt($field, $words = 55) {
