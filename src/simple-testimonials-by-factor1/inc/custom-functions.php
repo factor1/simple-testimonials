ce297f8f32acd431633afe08c6fcbb7e7cc6c7fb
Melanie <mlpatt23@gmail.com>
2018-02-22 16:19:48 -0700

    Fixed default parameter issue

--- src/simple-testimonials-by-factor1/inc/custom-functions.php
+++ src/simple-testimonials-by-factor1/inc/custom-functions.php
@@ -1,19 +1,20 @@
 <?php
 /*
-* Custom functions 
+* Custom functions
 *
 */
 
 // Custom Excerpt function for Advanced Custom Fields (For Tesimonials Excerpts)
 function simple_testimonials_excerpt($field, $words = 55) {
   global $post;
-  if ( '' != $field ) {
-    $field = strip_shortcodes( $field );
-    $field = apply_filters('the_content', $field);
-    $field = str_replace(']]&gt;', ']]&gt;', $field);
+  $text = $field;
+  if ( '' != $text ) {
+    $text = strip_shortcodes( $text );
+    $text = apply_filters('the_content', $text);
+    $text = str_replace(']]&gt;', ']]&gt;', $text);
     $excerpt_length = $words;
     $excerpt_more = apply_filters('excerpt_more', ' ' . '[...]');
-    $text = wp_trim_words( $field, $excerpt_length, $excerpt_more );
+    $text = wp_trim_words( $text, $excerpt_length, $excerpt_more );
   }
-  return apply_filters('the_excerpt', $field);
-} ?>
+  return apply_filters('the_excerpt', $text);
+}  ?>
